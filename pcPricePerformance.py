1cf5fdb15718382875f9a282a1a29013a8da80a9
Adrian Kelly <ahalekelly@gmail.com>
2020-05-29 06:03:33 -0700

    plotly chart

--- pcPricePerformance.py
+++ pcPricePerformance.py
@@ -4,12 +4,15 @@ import os
 import csv
 import re
 from datetime import datetime
+import plotly.graph_objects as go
+
 
 scrapeWeb = True
 mode = "both"
 
 cpuBlacklists = re.compile(r'i[3-9]-[2-7]|i[3-9]-\d{3}$|FX-|1300X|1400|1500X|2400G')
 gpuBlacklists = re.compile(r'Quadro|GT |HD |GTX [3-9]|R[5-9]|Titan|GTX 10|RX 4')
+colorMap = {'AMD':'rgb(255, 65, 54)','Intel':'rgb(93, 164, 214)','Nvidia':'rgb(44, 160, 101)'}
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 print(os.path.abspath("."))
@@ -48,19 +51,50 @@ if mode != "gpu":
         for ulCPU in ulCPUs:
             if (model.lower() in ulCPU["model"].lower() or ulCPU["model"].lower() in model.lower()) and ulCPU["performance"] > 0:
                 modelShort = model.replace("Core ","")\
+                .replace("Pentium ","")\
+                .replace("Threadripper ","")\
                 .replace("Ryzen 3 ","")\
                 .replace("Ryzen 5 ","")\
                 .replace("Ryzen 7 ","")\
                 .replace("Ryzen 9 ","")
                 matches[modelShort] = {**pcppCPUs[model], **ulCPU}
-#                print(model, matches[model])
     print(len(matches), "matches")
+    prices=[]
+    scores=[]
+    popularities=[]
+    names=[]
+    fullNames=[]
+    brands=[]
+    maxPrice = 750
+    print(maxPrice)
 
     with open(os.path.join(os.path.dirname(__file__), 'cpus-'+date+'.csv'), 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow(["Model","Price","Performance","Brand","Popularity","Cores"])
         for model in matches:
             spamwriter.writerow([model, str(matches[model]["price"]), matches[model]["performance"], matches[model]["brand"], matches[model]["popularity"], matches[model]["cores"]])
+            if matches[model]["price"] <= maxPrice:
+                prices.append(str(matches[model]["price"]))
+                scores.append(matches[model]["performance"])
+                popularities.append(matches[model]["popularity"])
+                names.append(model)
+                fullNames.append(matches[model]["model"])
+                brands.append(matches[model]["brand"])
+    fig = go.Figure(data=[go.Scatter(
+        x=prices, y=scores,
+        text=names,
+        mode='markers+text',
+        textposition='middle center',
+        marker=dict(
+            color=[colorMap[brand] for brand in brands],
+            size=popularities,
+            sizemode='area',
+            sizeref=2.*max(popularities)/(60.**2),
+            sizemin=5 
+        )
+    )])
+    print(prices[scores.index(max(scores))])
+    fig.show()
 
 if mode != "cpu":
     if scrapeWeb:
